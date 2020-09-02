785ba7d83732b7f852920773aa850962a40ce2e3
yinjijie <yinjijie@jinshiedu.net>
2020-09-02 12:29:09 +0800

    initialize

---
+++ src/main/java/com/season/seasonStudy/SeasonStudyApplication.java
@@ -0,0 +1,13 @@
+package com.season.seasonStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeasonStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/season/seasonStudy/SeasonStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.season.seasonStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
